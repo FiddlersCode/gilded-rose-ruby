a2a71eeaf6544c0978781f62afa5fa54844a3663
Paula Muldoon <pmuldoon@umich.edu>
2017-06-15 13:05:36 +0100

    quality degrading 2x fast test passes

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,7 +1,7 @@
 require 'gilded_rose'
 
 describe GildedRose do
-  let(:item) { Item.new("foo", 2, 2)}
+  let(:item) { Item.new("foo", 2, 6)}
   let(:items) { [] }
   let(:gilded_rose) { GildedRose.new(items) }
 
@@ -20,6 +20,11 @@ describe GildedRose do
     it 'decreases sell_in by 1' do
       2.times { gilded_rose.update_quality }
       expect(items[0].sell_in).to eq -1
+    end
+
+    it 'quality degrades twice as fast after sell_in < 0' do
+       2.times { gilded_rose.update_quality }
+       expect(items[0].quality).to eq 2
     end
 
   end
