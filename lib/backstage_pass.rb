a2a74359d20256156068b1a0d61b894f8004d771
Paula Muldoon <pmuldoon@umich.edu>
2017-06-15 17:21:38 +0100

    backstage creation tests pass

--- lib/backstage_pass.rb
+++ lib/backstage_pass.rb
@@ -1,7 +1,9 @@
 class BackstagePass
-  attr_reader :name
+  attr_reader :name, :sell_in, :quality
 
-  def initialize(name)
+  def initialize(name, sell_in, quality)
     @name = name
+    @sell_in = sell_in
+    @quality = quality
   end
 end
--- spec/backstage_pass_spec.rb
+++ spec/backstage_pass_spec.rb
@@ -1,10 +1,18 @@
 require 'backstage_pass'
 
 describe 'backstage_pass' do
-  let(:pass) { BackstagePass.new("Beatles pass") }
+  let(:pass) { BackstagePass.new("Beatles pass", 11, 3) }
 
   it 'can be created with a name' do
     expect(pass.name).to eq "Beatles pass"
+  end
+
+  it 'can be created with a sell_in' do
+    expect(pass.sell_in).to eq 11
+  end
+
+  it 'can be created with a quality' do
+    expect(pass.quality).to eq 3
   end
 
 end
