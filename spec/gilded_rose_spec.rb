ac04d2b5fe049166ee81a36bdaca2a401a5883ed
Paula Muldoon <pmuldoon@umich.edu>
2017-06-15 13:02:49 +0100

    decrease sell-in test passes

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,19 +1,26 @@
 require 'gilded_rose'
 
 describe GildedRose do
-  let(:item) { Item.new("foo", 0, 0)}
+  let(:item) { Item.new("foo", 2, 2)}
   let(:items) { [] }
   let(:gilded_rose) { GildedRose.new(items) }
 
-  before :each do
-    items << item
-  end
+
 
   describe "#update_quality" do
-    it "does not change the name" do
+    before :each do
+      items << item
       gilded_rose.update_quality
+    end
+
+    it "does not change the name" do
       expect(items[0].name).to eq "foo"
     end
-  end
 
+    it 'decreases sell_in by 1' do
+      2.times { gilded_rose.update_quality }
+      expect(items[0].sell_in).to eq -1
+    end
+
+  end
 end
