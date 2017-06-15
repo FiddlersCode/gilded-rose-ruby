de108a408c331a5645e1bfda610e76bea1a2a5ec
Paula Muldoon <pmuldoon@umich.edu>
2017-06-15 12:42:07 +0100

    refactored test, added lets

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -1,11 +1,14 @@
 require 'gilded_rose'
 
 describe GildedRose do
+  let(:item) { Item.new("foo", 0, 0)}
+  let(:items) { [] }
+  let(:gilded_rose) { GildedRose.new(items) }
 
   describe "#update_quality" do
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
+      items << item
+      gilded_rose.update_quality
       expect(items[0].name).to eq "foo"
     end
   end
