008b2c67d0c1d349dd735c849709e97a540199db
Paula Muldoon <pmuldoon@umich.edu>
2017-06-15 12:46:07 +0100

    added before hook to test

--- spec/gilded_rose_spec.rb
+++ spec/gilded_rose_spec.rb
@@ -5,9 +5,12 @@ describe GildedRose do
   let(:items) { [] }
   let(:gilded_rose) { GildedRose.new(items) }
 
+  before :each do
+    items << item
+  end
+
   describe "#update_quality" do
     it "does not change the name" do
-      items << item
       gilded_rose.update_quality
       expect(items[0].name).to eq "foo"
     end
